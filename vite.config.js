26f299898adeb55726265139574d895e7721f7cf
123 <1447143618@qq.com>
2026-04-29 22:11:48 +0100

    Deploy Vite app to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
+  base: "/spectra-ppt/",
   plugins: [react(), tailwindcss()],
   server: {
     watch: {
